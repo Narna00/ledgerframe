f65e44a7349bff1bc5b9000c5ce1bc0391085938
Prince Amoako Atta <princeamoakoatta22@gmail.com>
2026-06-26 23:18:48 +0000

    Update build.sh

--- .clusterfuzzlite/build.sh
+++ .clusterfuzzlite/build.sh
@@ -3,12 +3,25 @@
 COMMON_SOURCES=(
   "$SRC/src/batch_reader.cpp"
   "$SRC/src/bitstream.cpp"
+  "$SRC/src/catalog.cpp"
   "$SRC/src/crc32.cpp"
   "$SRC/src/frame_reader.cpp"
   "$SRC/src/index.cpp"
+  "$SRC/src/inspect.cpp"
+  "$SRC/src/aggregate.cpp"
+  "$SRC/src/manifest.cpp"
+  "$SRC/src/normalize.cpp"
+  "$SRC/src/query.cpp"
+  "$SRC/src/repair.cpp"
+  "$SRC/src/retention.cpp"
+  "$SRC/src/schema.cpp"
+  "$SRC/src/segment.cpp"
+  "$SRC/src/timeline.cpp"
+  "$SRC/src/varint.cpp"
+  "$SRC/src/writer.cpp"
 )
 
-for target in frame batch index; do
+for target in frame batch index query schema writer repair; do
   "$CXX" $CXXFLAGS -std=c++17 -I"$SRC/include" \
     "$SRC/fuzz/${target}_fuzzer.cc" "${COMMON_SOURCES[@]}" \
     $LIB_FUZZING_ENGINE -o "$OUT/${target}_fuzzer"
